6e73aac2d80b98a531fdd28fb8a42529876afe62
bohandre <bohandre24@gmail.com>
2021-12-13 14:08:53 -0500

    update js file

--- figuras.js
+++ figuras.js
@@ -2,14 +2,29 @@
 
 console.group("cuadrado");
 
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+// const ladoCuadrado = 5;
+// console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es " + perimetroCuadrado + " cm");
+// const perimetroCuadrado = ladoCuadrado * 4;
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El Área del cuadrado es " + areaCuadrado + " cm²");
+function perimetroCuadrado(lado)
+{
+    return lado * 4;
+}
+
+perimetroCuadrado()
+
+// console.log("El perímetro del cuadrado es " + perimetroCuadrado + " cm");
+
+// const areaCuadrado = ladoCuadrado * ladoCuadrado;
+// console.log("El Área del cuadrado es " + areaCuadrado + " cm²");
+
+function areaCuadrado(lado)
+{
+    return lado * lado;
+}
+
+areaCuadrado()
 
 console.groupEnd();
 
@@ -17,28 +32,42 @@ console.groupEnd();
 
 console.group("Triangulo");
 
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
+// const ladoTriangulo1 = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
 
-console.log(
-    "Los lados del Triángulo miden: " 
-    + ladoTriangulo1 
-    + " cm, " 
-    + ladoTriangulo2 
-    + " cm, " 
-    + baseTriangulo 
-    + " cm."
-    );
+// console.log(
+//    "Los lados del Triángulo miden: " 
+//   + ladoTriangulo1 
+//    + " cm, " 
+//    + ladoTriangulo2 
+//    + " cm, "
+//    + baseTriangulo 
+//    + " cm."
+//    );
 
-const alturaTriangulo = 5.5;
-console.log("La altura del Triangulo es " + alturaTriangulo + " cm.");
+// const alturaTriangulo = 5.5;
+// console.log("La altura del Triangulo es " + alturaTriangulo + " cm.");
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log("El perímetro del Triangulo es " + perimetroTriangulo + " cm.");
+function perimetroTriangulo(lado1, lado2, base)
+{
+    return lado1 + lado2 + base;
+}
 
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2; 
-console.log("El área del Triangulo es " + areaTriangulo + " cm².");
+perimetroTriangulo()
+
+// console.log("El perímetro del Triangulo es " + perimetroTriangulo + " cm.");
+
+// const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2; 
+
+function areaTriangulo(base, altura)
+{
+    return (base * altura) / 2;
+}
+
+areaTriangulo()
+
+// console.log("El área del Triangulo es " + areaTriangulo + " cm².");
 
 console.groupEnd();
 
@@ -47,13 +76,21 @@ console.groupEnd();
 console.group("Círculo");
 
 // Radio
-const radioCirculo = 4;
-console.log("El radio del Circulo es " + radioCirculo + " cm².");
+// const radioCirculo = 4;
+// console.log("El radio del Circulo es " + radioCirculo + " cm².");
 
 // Diámetro
 
-const diametroCirculo = radioCirculo * 2;
-console.log("El diámetro del Circulo es " + diametroCirculo + " cm².");
+// const diametroCirculo = radioCirculo * 2;
+
+function diametroCirculo(radio)
+{
+    return radio * 2;
+}
+
+diametroCirculo()
+
+// console.log("El diámetro del Circulo es " + diametroCirculo + " cm².");
 
 // PI
 
@@ -62,13 +99,28 @@ console.log("PI es " + PI);
 
 // Circunferencia
 
-const perimetroCirculo = diametroCirculo * PI;
-console.log("El perimetro del Circulo es " + perimetroCirculo + " cm².");
+// const perimetroCirculo = diametroCirculo * PI;
+
+function perimetroCirculo(radio)
+{
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+
+perimetroCirculo()
+
+// console.log("El perimetro del Circulo es " + perimetroCirculo + " cm².");
 
 // Área
 
-const areaCirculo = PI * ( radioCirculo * radioCirculo); 
-console.log("El área del Circulo es " + areaCirculo + " cm².");
+// const areaCirculo = PI * ( radioCirculo * radioCirculo); 
+
+function areaCirculo(radio)
+{
+    return PI * (radio * radio) * PI; 
+}
+
+// console.log("El área del Circulo es " + areaCirculo + " cm².");
 
 console.groupEnd();
 
